865fac976f6e19775ab64907148cd256640266d0
MIchael Berry <michaeladamberry3@gmail.com>
2019-01-24 21:34:38 -0500

    inputs should follow accessability best practices

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,11 @@ export default class Home extends React.Component {
             return (
               <div className="homeSearch">
                 <form onSubmit={actions.submitQuery}>
-                  <input
+                  <label htmlFor="homeSearch">Search for a book title</label>
+                  <input 
+                    className="homeSearch"
+                    aria-label="Search for a book title"
+                    aria-required="true"
                     type="text"
                     value={query}
                     onChange={actions.typeLetter}
